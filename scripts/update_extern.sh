faa9d1ac0c882663b77260bb3f3c372f28c0a18f
John Chilton <jmchilton@gmail.com>
2015-02-20 16:08:50 -0500

    WIP on XSD validation.

--- scripts/update_extern.sh
+++ scripts/update_extern.sh
@@ -9,3 +9,8 @@ for file in 'rgToolFactory2.xml' 'rgToolFactory2.py' 'getlocalrpackages.py' 'LIC
 do
     wget "https://raw.githubusercontent.com/galaxyproject/tools-iuc/master/tools/tool_factory_2/$file"  --output-document "extern/tool_factory_2/$file"
 done
+
+for file in 'galaxy.xsd' 'citations.xsd' 'citation.xsd' 'LICENSE';
+do
+    wget "https://raw.githubusercontent.com/JeanFred/Galaxy-XSD/master/$file" --output-document "extern/xsd/$file"
+done
